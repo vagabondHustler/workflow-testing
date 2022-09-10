828ab98ab6c4c872c5124122b9bd556b99426698
vagabondHustler <vagabondhustler.github@gmail.com>
2022-09-11 01:12:31 +0200

    asdasd

--- scripts/reformat_changelog.py
+++ scripts/reformat_changelog.py
@@ -2,7 +2,7 @@ import re
 import sys
 
 back_tick = "\n```"
-content = sys.arg[-1]
+content = sys.argv[-1]
 expression = "(?!\*\n)\v(\n)"
 pattern = re.compile(expression)
 new_content = pattern.sub(back_tick, content)
