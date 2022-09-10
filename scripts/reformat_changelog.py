818d7463377301d6fd503872817aef9257eac881
vagabondHustler <vagabondhustler.github@gmail.com>
2022-09-11 01:06:25 +0200

    23qwdwas

---
+++ scripts/reformat_changelog.py
@@ -0,0 +1,9 @@
+import re
+import sys
+
+back_tick = "\n```"
+content = sys.arg[-1]
+expression = "(?!\*\n)\v(\n)"
+pattern = re.compile(expression)
+new_content = pattern.sub(back_tick, content)
+print(new_content)
