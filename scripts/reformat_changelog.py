652833466d624c4e979c13847892ad703f5294e4
vagabondHustler <vagabondhustler.github@gmail.com>
2022-09-11 02:06:25 +0200

    grsdfdsfsf

--- scripts/reformat_changelog.py
+++ scripts/reformat_changelog.py
@@ -1,9 +1,16 @@
+import os
 import re
 import sys
 
-back_tick = "\n```"
-content = sys.argv[-1]
-expression = "(?!\*\n)\v(\n)"
-pattern = re.compile(expression)
-new_content = pattern.sub(back_tick, content)
-print(new_content)
+cwd = os.getcwd()
+back_tick = "\\n\\n```\\n\\n"
+file_path = f"{cwd}/changelog.md"
+
+with open(file_path, "r+", encoding="utf-8") as f:
+    file_content = f.read()
+    expression = "(\\n\\n)"
+    pattern = re.compile(expression)
+    new_content = pattern.sub(back_tick, file_content)
+    f.seek(0)
+    f.truncate()
+    f.write(new_content)
